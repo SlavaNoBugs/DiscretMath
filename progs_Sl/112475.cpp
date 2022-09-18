f30641d415150e48f1523dc465c0018245f43516
Zagvozdkin_Vyacheslav <zagvozdkin12@gmail.com>
2022-09-18 21:17:27 +0300

    3 номера

---
+++ progs_Sl/112475.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string str;
+    std::getline(std::cin, str);
+    bool fl = true;
+    int length = str.length();
+    if ((str[0] >= '0' && str[0] <= '9') || !((str[0] >= 'A' && str[0] <= 'Z') || (str[0] >= 'a' && str[0] <= 'z') || str[0] == '_') || str[0] == NULL)
+    {
+        fl = false;
+    }
+    else
+    {
+        for (int i = 0; i < length; i++)
+        {
+            if ((str[i] >= '0' && str[i] <= '9') || (str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z') || str[i] == '_')
+            {
+                continue;
+            }
+            else fl = false;
+        }
+    }
+    if (fl)std::cout << "YES";
+    else std::cout << "NO";
+}
